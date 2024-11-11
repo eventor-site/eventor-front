00d1f4a9ce000e5c38cd96ae72451b2f5a406b9b
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-11-12 02:08:29 +0900

    feat: 이전 프로젝트 파일 복제

---
+++ src/main/java/com/eventorfront/EventorFrontApplication.java
@@ -0,0 +1,13 @@
+package com.eventorfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventorFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventorFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventorfront/EventorFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventorfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventorFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
