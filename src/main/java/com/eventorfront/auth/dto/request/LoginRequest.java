e63eb32e8faf6da26b7af42c0e2d0a7817cc5c38
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-11-18 01:28:38 +0900

    feat: id 변수명 identifier로 변경

--- src/main/java/com/eventorfront/auth/dto/request/LoginRequest.java
+++ src/main/java/com/eventorfront/auth/dto/request/LoginRequest.java
@@ -1,6 +1,6 @@
 package com.eventorfront.auth.dto.request;
 
 public record LoginRequest(
-	String id,
+	String identifier,
 	String password) {
 }
--- src/main/java/com/eventorfront/auth/dto/request/SignUpRequest.java
+++ src/main/java/com/eventorfront/auth/dto/request/SignUpRequest.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 
 @Builder
 public record SignUpRequest(
-	String id,
+	String identifier,
 	String password,
 	String name,
 	String nickName,
