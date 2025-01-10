da2e92693269ba1451df6bff24609525c42d14a1
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-01-10 13:12:54 +0900

    feat: 로그인 실패 메세지 출력

--- src/main/java/com/eventorfront/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/eventorfront/global/handler/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.eventorfront.global.handler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eventorfront.global.exception.UnauthorizedException;
 
@@ -17,7 +18,10 @@ public class GlobalExceptionHandler {
 	}
 
 	@ExceptionHandler(FeignException.Unauthorized.class)
-	public String handleNotFoundException() {
+	public String handleUnauthorizedException(RedirectAttributes redirectAttributes) {
+		// 리다이렉트 시 메시지 추가
+		redirectAttributes.addFlashAttribute("message", "인증에 실패 했습니다. 다시 로그인해 주세요");
+
 		// 로그인 페이지로 리다이렉트
 		return "redirect:/auth/login";
 	}
