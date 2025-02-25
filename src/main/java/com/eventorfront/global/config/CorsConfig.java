8c7e09a6460e34a79cecd6d5c77ecf38d73a3ce4
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-25 23:37:05 +0900

    feat: CorsConfig 수정

--- src/main/java/com/eventorfront/global/config/CorsConfig.java
+++ src/main/java/com/eventorfront/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")  // 모든 URL 에 대해 적용
-			.allowedOrigins("https://www.eventor.store.com")  // 특정 도메인만 허용
+			.allowedOrigins("https://www.eventor.store")  // 특정 도메인만 허용
 			.allowedMethods("GET", "POST", "PUT", "DELETE")  // 허용할 HTTP 메서드
 			.allowedHeaders("*")  // 모든 헤더 허용
 			.allowCredentials(true);  // 쿠키 포함 허용
