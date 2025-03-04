7c42c6bdfd1fad0799eaa9b992700f0a59219b07
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-04 21:50:05 +0900

    feat: index 페이지 수정, 카테고리 js, css 공통 분리

--- src/main/resources/static/js/common/category-menu.js
+++ src/main/resources/static/js/common/category-menu.js
@@ -1,8 +1,3 @@
-// document.getElementById('category-button').addEventListener('click', function () {
-//     document.querySelector('.category-button').classList.toggle('active');
-// });
-
-// 마우스 이벤트로 하위 카테고리 표시
 document.querySelectorAll('.category-item').forEach(item => {
     item.addEventListener('mouseenter', function () {
         const subCategory = this.querySelector('.sub-category');
