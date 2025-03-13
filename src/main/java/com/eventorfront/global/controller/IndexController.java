bf238f5f91954223bf0210a28491deea5791e3d6
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-14 03:38:17 +0900

    feat: 블루/그린 배포 구현중

--- src/main/java/com/eventorfront/global/controller/IndexController.java
+++ src/main/java/com/eventorfront/global/controller/IndexController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import com.eventorfront.bookmark.service.BookmarkService;
 import com.eventorfront.post.service.PostService;
 
-import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 
-@lombok.extern.slf4j.Slf4j
-@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
@@ -25,7 +22,6 @@ public class IndexController {
 
 	@GetMapping("/")
 	public String mainPage(Model model) {
-		log.info("Main Page 실행 ");
 		model.addAttribute("hotPosts", postService.getHotEventPosts().getData());
 		model.addAttribute("latestPosts", postService.getLatestEventPosts().getData());
 		model.addAttribute("deadlinePosts", postService.getDeadlineEventPosts().getData());
