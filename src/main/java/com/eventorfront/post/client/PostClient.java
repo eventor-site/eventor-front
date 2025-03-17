c82dc7692ca5bdf9848e209d7556f3af9cc66ce1
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-17 19:29:12 +0900

    feat: 관리자 기간별 이벤트 작성 게시물 조회 날짜 형식 수정

--- src/main/java/com/eventorfront/post/client/PostClient.java
+++ src/main/java/com/eventorfront/post/client/PostClient.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -99,5 +100,6 @@ public interface PostClient {
 
 	@GetMapping("/back/posts/statistic/users/admin")
 	ResponseEntity<ApiResponse<List<GetEventPostCountByAdminResponse>>> getEventPostCountByAdmin(
-		@RequestParam LocalDateTime startTime, @RequestParam LocalDateTime endTime);
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime startTime,
+		@RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime endTime);
 }
