ccf1785e72734fc16a765c1b426de5c65817f72d
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-03-18 17:51:22 +0900

    feat: 게시글 추천/비추천 이미지 추가, 메인페이지에 로그인한 사용자만 즐겨찾기 리스트 출력되도록 수정

--- src/main/java/com/eventorfront/global/controller/IndexController.java
+++ src/main/java/com/eventorfront/global/controller/IndexController.java
@@ -4,9 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.eventorfront.auth.service.AuthService;
 import com.eventorfront.bookmark.service.BookmarkService;
 import com.eventorfront.post.service.PostService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -14,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 public class IndexController {
 	private final PostService postService;
 	private final BookmarkService bookmarkService;
+	private final AuthService authService;
 
 	// @GetMapping("/")
 	// public String indexPage() {
@@ -21,7 +24,8 @@ public class IndexController {
 	// }
 
 	@GetMapping("/")
-	public String mainPage(Model model) {
+	public String mainPage(HttpServletRequest request, Model model) {
+		model.addAttribute("isLogin", authService.hasTokensInCookie(request));
 		model.addAttribute("hotPosts", postService.getHotEventPosts().getData());
 		model.addAttribute("latestPosts", postService.getLatestEventPosts().getData());
 		model.addAttribute("deadlinePosts", postService.getDeadlineEventPosts().getData());
