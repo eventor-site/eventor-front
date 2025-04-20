8c842051c723c09a7029d3914e86593c6cdb40ef
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-04-21 03:03:50 +0900

    feat: 커스텀 버튼 하단 끝 도달시 안보이게, 정책 화면 모바일 반응형 추가

--- src/main/resources/static/js/common/custom-button.js
+++ src/main/resources/static/js/common/custom-button.js
@@ -16,7 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // 스크롤 이벤트
     window.addEventListener("scroll", function () {
-        const shouldShow = window.scrollY > 100;
+        const scrollY = window.scrollY;
+        const windowHeight = window.innerHeight;
+        const documentHeight = document.documentElement.scrollHeight;
+
+        const isAtBottom = scrollY + windowHeight >= documentHeight - 5; // 오차 보정
+        const shouldShow = scrollY > 100 && !isAtBottom;
+
         scrollTopBtn.classList.toggle("show", shouldShow);
         scrollBottomBtn.classList.toggle("show", shouldShow);
     });
