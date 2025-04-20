83e874fbff1766cce91b5e2f3f2e5b1bacc6d151
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-04-20 22:09:29 +0900

    feat: 상태 0인 에러 패스

--- src/main/resources/static/js/common/errorHandler.js
+++ src/main/resources/static/js/common/errorHandler.js
@@ -6,10 +6,9 @@ window.fetch = async (...args) => {
     try {
         const response = await originalFetch(...args);
 
-        if (!response.ok) {
+        if (!response.ok && response.status !== 0) {
             // 응답 본문을 한 번만 읽어서 저장
             responseBody = await response.text();
-            const knownErrorStatuses = [401, 403, 404, 413, 500];
 
             if (response.status === 401) {
                 alert("🔒" + responseBody);
@@ -18,7 +17,7 @@ window.fetch = async (...args) => {
 
             }
             // 다른 에러 처리
-            else if (knownErrorStatuses.includes(response.status)) {
+            else {
                 alert(`❌ 요청 실패 (${response.status}): ${responseBody}`);
             }
         }
