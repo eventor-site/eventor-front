4390e715a9ed143609cb20c6d0e76a7d80cfdc8d
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-04-30 16:44:25 +0900

    refactor: 공유버튼 리팩토링

--- src/main/resources/static/js/post/share-button.js
+++ src/main/resources/static/js/post/share-button.js
@@ -1,18 +1,7 @@
-function shareX() {
-    var sendText = "이벤터"; // 전달할 텍스트
-    var sendUrl = window.location.href; // 전달할 URL
-    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(sendText) + "&url=" + encodeURIComponent(sendUrl));
-}
-
-function shareFacebook() {
-    var sendUrl = window.location.href; // 전달할 URL
-    window.open("http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(sendUrl));
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-    const postTitle = /*[[${post.title}]]*/ '제목';
-    const postContent = /*[[${post.content}]]*/ '내용';
-    const thumbnailUrl = /*[[${thumbnailUrl}]]*/ '썸네일';
+    const postTitle = document.getElementById('postTitle').innerText;
+    const postContent = document.getElementById('postContent').innerText;
+    const thumbnailUrl = document.getElementById('thumbnailUrl').innerText;
 
     const tempDiv = document.createElement('div');
     tempDiv.innerHTML = postContent;
@@ -45,19 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         ]
     });
-
-    const copyBtn = document.getElementById("copyBtn");
-
-    if (copyBtn) {
-        copyBtn.addEventListener("click", function () {
-            const url = window.location.href;
-
-            // 클립보드에 복사
-            navigator.clipboard.writeText(url).then(() => {
-                alertMessage("링크가 복사되었습니다!");
-            }).catch((err) => {
-                alertMessage("복사에 실패했습니다.");
-            });
-        });
-    }
 });
+
+function shareX() {
+    const sendText = document.getElementById('postTitle').innerText; // 전달할 텍스트
+    const sendUrl = window.location.href; // 전달할 URL
+    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(sendText) + "&url=" + encodeURIComponent(sendUrl));
+}
+
+function shareFacebook() {
+    const sendUrl = window.location.href; // 전달할 URL
+    window.open("http://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(sendUrl));
+}
+
+function copy() {
+    const url = window.location.href;
+
+    // 클립보드에 복사
+    navigator.clipboard.writeText(url).then(() => {
+        alertMessage("링크가 복사되었습니다!");
+    }).catch((err) => {
+        alertMessage("복사에 실패했습니다.");
+    });
+}
