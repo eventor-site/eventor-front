0def4486d7a5a5699c13920386be0a4e51f5d50e
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-05-05 16:55:33 +0900

    feat: 헤더 사이트 글씨 수정

--- src/main/resources/static/js/auth/loginCheck.js
+++ src/main/resources/static/js/auth/loginCheck.js
@@ -29,3 +29,9 @@ document.addEventListener("DOMContentLoaded", function () {
             });
     }
 });
+
+// 부모 창에서 호출되는 함수
+function openOAuthWindow(provider) {
+    let authUrl = "/auth/oauth2/authorization/" + provider;
+    window.open(authUrl, "_blank", "width=800,height=1000");
+}
