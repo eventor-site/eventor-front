8fc84fa86958ea74c64b3d256383822753576ff7
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-05-14 00:02:23 +0900

    feat: actuator 수정

--- src/main/java/com/eventorfront/actuator/controller/ActuatorController.java
+++ src/main/java/com/eventorfront/actuator/controller/ActuatorController.java
@@ -1,43 +1,33 @@
 package com.eventorfront.actuator.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.eventorfront.actuator.service.ActuatorService;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 public class ActuatorController {
-	private final ActuatorService actuatorService;
 
 	@Value("${server.port}")
 	private int port;
 
 	@GetMapping("/actuator/health")
-	public Boolean checkFrontHealth() {
-		return true;
+	public ResponseEntity<Boolean> checkFrontHealth() {
+		return ResponseEntity.ok(true);
 	}
 
 	@GetMapping("/actuator/version")
-	public String getFrontVersion() {
+	public ResponseEntity<String> getFrontVersion() {
+		String version;
 		if (port == 8001 || port == 8002) {
-			return "blue";
+			version = "blue";
 		} else {
-			return "green";
+			version = "green";
 		}
+
+		return ResponseEntity.ok(version);
 	}
-
-	// @GetMapping("/back/actuator/health")
-	// public Boolean checkBackendHealth() {
-	// 	return actuatorService.backCheckHealth().getData();
-	// }
-	//
-	// @GetMapping("/back/actuator/version")
-	// public String checkBackVersion() {
-	// 	return actuatorService.backCheckVersion().getData();
-	// }
-
 }
