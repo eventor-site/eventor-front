0e69e02356497cea67bc3fa7521a178c56d3a562
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-05-24 13:30:27 +0900

    feat: 메인 광고 제거

--- src/main/resources/static/js/advertisement/main.js
+++ src/main/resources/static/js/advertisement/main.js
@@ -1,39 +1,39 @@
-function setCookie(name, value, hours) {
-    const date = new Date();
-    date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
-    document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
-}
-
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
-
-
-function showAdModal() {
-    fetch("/advertisement/eventModal")
-        .then(response => response.text())
-        .then(html => {
-            document.body.insertAdjacentHTML("beforeend", html);
-
-            const modalEl = document.getElementById("eventModal");
-            const eventModal = new bootstrap.Modal(modalEl);
-            eventModal.show();
-
-            // 이벤트 바인딩
-            modalEl.querySelector("#dismissTodayBtn")?.addEventListener("click", function () {
-                setCookie("hideAdToday", "true", 24);
-                eventModal.hide();
-            });
-        })
-        .catch(err => {
-            console.error("광고 모달 로딩 실패:", err);
-        });
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    if (!getCookie("hideAdToday")) {
-        showAdModal();
-    }
-});
+// function setCookie(name, value, hours) {
+//     const date = new Date();
+//     date.setTime(date.getTime() + (hours * 60 * 60 * 1000));
+//     document.cookie = `${name}=${value}; expires=${date.toUTCString()}; path=/`;
+// }
+//
+// function getCookie(name) {
+//     const value = `; ${document.cookie}`;
+//     const parts = value.split(`; ${name}=`);
+//     if (parts.length === 2) return parts.pop().split(';').shift();
+// }
+//
+//
+// function showAdModal() {
+//     fetch("/advertisement/eventModal")
+//         .then(response => response.text())
+//         .then(html => {
+//             document.body.insertAdjacentHTML("beforeend", html);
+//
+//             const modalEl = document.getElementById("eventModal");
+//             const eventModal = new bootstrap.Modal(modalEl);
+//             eventModal.show();
+//
+//             // 이벤트 바인딩
+//             modalEl.querySelector("#dismissTodayBtn")?.addEventListener("click", function () {
+//                 setCookie("hideAdToday", "true", 24);
+//                 eventModal.hide();
+//             });
+//         })
+//         .catch(err => {
+//             console.error("광고 모달 로딩 실패:", err);
+//         });
+// }
+//
+// document.addEventListener("DOMContentLoaded", function () {
+//     if (!getCookie("hideAdToday")) {
+//         showAdModal();
+//     }
+// });
