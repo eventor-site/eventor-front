3177bcd1ecc02c33f0a50fd413ec26a20c1af075
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-05-30 03:27:44 +0900

    feat: 여행 상세 정보 페이지 디자인 수정

---
+++ src/main/resources/static/js/tour/slider.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    $('.event-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        swipeToSlide: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-square-caret-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-square-caret-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
