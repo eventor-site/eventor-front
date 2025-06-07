d4c62c9f7c49c5a247a19c3610925bf11416c35d
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-06-07 21:09:38 +0900

    feat: 게시물 조회 통계정보 추가

--- src/main/java/com/eventorfront/statistic/dto/response/GetStatistic.java
+++ src/main/java/com/eventorfront/statistic/dto/response/GetStatistic.java
@@ -5,7 +5,8 @@ import java.time.LocalDate;
 public record GetStatistic(
 	LocalDate date,
 	Long visitorCount,
-	Long visitedCount,
+	Long mainViewCount,
+	Long postViewCount,
 	Long loginCount,
 	Long signupCount
 ) {
